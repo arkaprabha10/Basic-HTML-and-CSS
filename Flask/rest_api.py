e2fa91fccc52aa892af690e2026d72a60920b5a8
Arkaprbha Banerjee <44334390+arkaprabha10@users.noreply.github.com>
2021-05-13 09:56:07 +0530

    simple js browser game

--- Flask/rest_api.py
+++ Flask/rest_api.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import flask
 from flask import request, jsonify
 app = flask.Flask(__name__)
@@ -29,40 +29,31 @@ def home():
 @app.route('/trial_path', methods=['GET'])
 def api_all():
     return jsonify(books)
+def trial_func():
+    return jsonify(123)
+
+@app.route('/trial_path2', methods=['GET'])
+def api_id():
+    # Check if an ID was provided as part of the URL.
+    # If ID is provided, assign it to a variable.
+    # If no ID is provided, display an error in the browser.
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id field provided. Please specify an id."
+
+    # Create an empty list for our results
+    results = []
+
+    # Loop through the data and match results that fit the requested ID.
+    # IDs are unique, but other fields might return many results
+    for book in books:
+        if book['id'] == id:
+            results.append(book)
+
+    # Use the jsonify function from Flask to convert our list of
+    # Python dictionaries to the JSON format.
+    return jsonify(results)
 
 
-=======
-import flask
-from flask import request, jsonify
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
-
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
-
-@app.route('/', methods=['GET'])
-def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
-
-@app.route('/trial_path', methods=['GET'])
-def api_all():
-    return jsonify(books)
-
-
->>>>>>> 0dfd02d123dae025b5a472f34e473f5e4dfb3e2f
 app.run()
